02f51a995ce1925e87ff848bc043151747a6d75e
Spingkee <pingkee317@gmail.com>
2020-03-10 19:56:50 +0100

    first commit

---
+++ src/Test/Test.redux.js
@@ -0,0 +1,33 @@
+import React  from 'react';
+import { connect } from "react-redux";
+import { push } from "react-router-redux";
+import { getData } from '../store/actions/GetDataAction/GetDataAction';
+
+class TestRedux extends React.Component {
+
+    render() {
+        return (
+            <div className="App">
+                <h1>I am a redux test page</h1>
+                <button onClick={() => {this.props.onGetData()}}>Render</button>
+            </div>
+        );
+    }
+}
+
+const state = (state, ownProps = {}) => {
+    return {
+        getDataReducer: state.getDataReducer,
+    }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        navigateTo: (location) => {
+            dispatch(push(location));
+        },
+        onGetData: () => dispatch(getData()),
+    }
+};
+
+export default connect(state, mapDispatchToProps)(TestRedux)
